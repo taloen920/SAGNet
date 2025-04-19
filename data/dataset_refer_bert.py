d5824f298df673be74f637262b10b137b2a4b2d3
檀恺 <1047979415@qq.com>
2025-04-19 20:50:20 +0800

    add files

---
+++ data/dataset_refer_bert.py
@@ -0,0 +1,173 @@
+import os
+import torch.utils.data as data
+import torch
+import numpy as np
+from PIL import Image
+import random
+from bert.tokenization_bert import BertTokenizer
+from refer.refer import REFER
+
+
+class ReferDataset(data.Dataset):
+    def __init__(self,
+                 args,
+                 image_transforms=None,
+                 target_transforms=None,
+                 split='train',
+                 eval_mode=False):
+
+        self.image_transforms = image_transforms
+        self.target_transform = target_transforms
+        self.split = split
+        self.refer = REFER(args.refer_data_root, args.dataset, args.splitBy)
+        self.dataset = args.dataset
+        self.max_tokens = 20
+        self.eval_mode = eval_mode
+
+        print('Preparing dataset ......')
+        print(args.dataset, split)
+
+        # 处理自定义数据集
+        if args.dataset in ['LandRef', 'rrsis', 'new1216']:
+            self.ref_ids = []
+            for ref in self.refer.data['refs'][split]:
+                self.ref_ids.append(ref['ref_id'])
+
+            # 获取图像信息
+            img_ids = list(set(ref['image_id'] for ref in self.refer.data['refs'][split]))
+            self.imgs = []
+            for img_id in img_ids:
+                self.imgs.append({
+                    'id': img_id,
+                    'file_name': f"{img_id}.tif"
+                })
+        else:
+            # 原有数据集处理
+            self.ref_ids = self.refer.getRefIds(split=self.split)
+            img_ids = self.refer.getImgIds(self.ref_ids)
+            all_imgs = self.refer.Imgs
+            self.imgs = list(all_imgs[i] for i in img_ids)
+
+        # 随机选择20%的图像进行mask
+        num_images_to_mask = int(len(self.ref_ids) * 0.2)
+        self.images_to_mask = random.sample(self.ref_ids, num_images_to_mask)
+
+        # BERT tokenizer处理
+        self.input_ids = []
+        self.attention_masks = []
+        self.tokenizer = BertTokenizer.from_pretrained(args.bert_tokenizer)
+
+        # 处理每个refer的文本描述
+        for r in self.ref_ids:
+            if args.dataset in ['LandRef', 'rrsis', 'new1216']:
+                ref = self.refer.Refs[r]
+                sentences_raw = [s['sent'] for s in ref['sentences']]
+            else:
+                ref = self.refer.Refs[r]
+                sentences_raw = [el['raw'] for el in ref['sentences']]
+
+            sentences_for_ref = []
+            attentions_for_ref = []
+
+            for sentence_raw in sentences_raw:
+                attention_mask = [0] * self.max_tokens
+                padded_input_ids = [0] * self.max_tokens
+
+                input_ids = self.tokenizer.encode(text=sentence_raw, add_special_tokens=True)
+                input_ids = input_ids[:self.max_tokens]  # 截断
+
+                padded_input_ids[:len(input_ids)] = input_ids
+                attention_mask[:len(input_ids)] = [1] * len(input_ids)
+
+                sentences_for_ref.append(torch.tensor(padded_input_ids).unsqueeze(0))
+                attentions_for_ref.append(torch.tensor(attention_mask).unsqueeze(0))
+
+            self.input_ids.append(sentences_for_ref)
+            self.attention_masks.append(attentions_for_ref)
+
+    def __len__(self):
+        return len(self.ref_ids)
+
+    def add_random_boxes(self, img, min_num=20, max_num=60, size=32):
+        h, w = size, size
+        img = np.asarray(img).copy()
+        img_size = img.shape[1]
+        num = random.randint(min_num, max_num)
+        for _ in range(num):
+            y, x = random.randint(0, img_size - w), random.randint(0, img_size - h)
+            img[y:y + h, x:x + w] = 0
+        return Image.fromarray(img.astype('uint8'), 'RGB')
+
+    def __getitem__(self, index):
+        this_ref_id = self.ref_ids[index]
+
+        if self.dataset in ['LandRef', 'rrsis', 'new1216']:
+            ref = self.refer.Refs[this_ref_id]
+            img_id = ref['image_id']
+            img_info = next(img for img in self.imgs if img['id'] == img_id)
+
+            # 加载图像
+            img_path = os.path.join(self.refer.IMAGE_DIR, img_info['file_name'])
+            img = Image.open(img_path).convert("RGB")
+
+            # 加载掩码
+            mask_path = os.path.join(self.refer.ROOT_DIR, f'data/{self.dataset}/masks', f"{img_id}.tif")
+            ref_mask = np.array(Image.open(mask_path))
+            ref_mask = (ref_mask[:, :, 0] > 0).astype(np.uint8)
+
+            annot = Image.fromarray(ref_mask, mode='P')
+        else:
+            this_img_id = self.refer.getImgIds(this_ref_id)
+            this_img = self.refer.Imgs[this_img_id[0]]
+
+            img = Image.open(os.path.join(self.refer.IMAGE_DIR, this_img['file_name'])).convert("RGB")
+            ref = self.refer.loadRefs(this_ref_id)
+            ref_mask = np.array(self.refer.getMask(ref[0])['mask'])
+            annot = np.zeros(ref_mask.shape)
+            annot[ref_mask == 1] = 1
+            annot = Image.fromarray(annot.astype(np.uint8), mode="P")
+
+        # 随机添加mask
+        if self.split == 'train' and this_ref_id in self.images_to_mask:
+            img = self.add_random_boxes(img)
+
+        # 应用图像变换
+        if self.image_transforms is not None:
+            img, target = self.image_transforms(img, annot)
+
+        # 处理文本嵌入
+        if self.eval_mode:
+            embedding = []
+            att = []
+            for s in range(len(self.input_ids[index])):
+                e = self.input_ids[index][s]
+                a = self.attention_masks[index][s]
+                embedding.append(e.unsqueeze(-1))
+                att.append(a.unsqueeze(-1))
+            tensor_embeddings = torch.cat(embedding, dim=-1)
+            attention_mask = torch.cat(att, dim=-1)
+        else:
+            choice_sent = np.random.choice(len(self.input_ids[index]))
+            tensor_embeddings = self.input_ids[index][choice_sent]
+            attention_mask = self.attention_masks[index][choice_sent]
+
+        return img, target, tensor_embeddings, attention_mask
+
+    def get_category_id(self, index):
+        """
+        获取指定索引的数据项对应的类别ID
+
+        参数:
+            index: 数据索引
+
+        返回:
+            category_id: 类别ID
+        """
+        # 获取对应的ref_id
+        ref_id = self.ref_ids[index]
+
+        # 从refer对象中获取对应的引用
+        ref = self.refer.loadRefs(ref_id)[0]
+
+        # 返回类别ID
+        return ref['category_id']
---
+++ utils.py
@@ -0,0 +1,210 @@
+from __future__ import print_function
+from collections import defaultdict, deque
+import datetime
+import math
+import time
+import torch
+import errno
+import os
+import sys
+
+
+class SmoothedValue(object):
+    """Track a series of values and provide access to smoothed values over a
+    window or the global series average.
+    """
+
+    def __init__(self, window_size=20, fmt=None):
+        if fmt is None:
+            fmt = "{median:.4f} ({global_avg:.4f})"
+        self.deque = deque(maxlen=window_size)
+        self.total = 0.0
+        self.count = 0
+        self.fmt = fmt
+
+    def update(self, value, n=1):
+        self.deque.append(value)
+        self.count += n
+        self.total += value * n
+
+    def synchronize_between_processes(self):
+        """
+        简化版：单卡模式下不需要同步，直接返回
+        """
+        return
+
+    @property
+    def median(self):
+        d = torch.tensor(list(self.deque))
+        return d.median().item()
+
+    @property
+    def avg(self):
+        d = torch.tensor(list(self.deque), dtype=torch.float32)
+        return d.mean().item()
+
+    @property
+    def global_avg(self):
+        return self.total / self.count
+
+    @property
+    def max(self):
+        return max(self.deque)
+
+    @property
+    def value(self):
+        return self.deque[-1]
+
+    def __str__(self):
+        return self.fmt.format(
+            median=self.median,
+            avg=self.avg,
+            global_avg=self.global_avg,
+            max=self.max,
+            value=self.value)
+
+
+class MetricLogger(object):
+    def __init__(self, delimiter="\t"):
+        self.meters = defaultdict(SmoothedValue)
+        self.delimiter = delimiter
+
+    def update(self, **kwargs):
+        for k, v in kwargs.items():
+            if isinstance(v, torch.Tensor):
+                v = v.item()
+            assert isinstance(v, (float, int))
+            self.meters[k].update(v)
+
+    def __getattr__(self, attr):
+        if attr in self.meters:
+            return self.meters[attr]
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        raise AttributeError("'{}' object has no attribute '{}'".format(
+            type(self).__name__, attr))
+
+    def __str__(self):
+        loss_str = []
+        for name, meter in self.meters.items():
+            loss_str.append(
+                "{}: {}".format(name, str(meter))
+            )
+        return self.delimiter.join(loss_str)
+
+    def synchronize_between_processes(self):
+        """
+        简化版：单卡模式下不需要同步
+        """
+        pass
+
+    def add_meter(self, name, meter):
+        self.meters[name] = meter
+
+    def log_every(self, iterable, print_freq, header=None):
+        i = 0
+        if not header:
+            header = ''
+        start_time = time.time()
+        end = time.time()
+        iter_time = SmoothedValue(fmt='{avg:.4f}')
+        data_time = SmoothedValue(fmt='{avg:.4f}')
+        space_fmt = ':' + str(len(str(len(iterable)))) + 'd'
+        log_msg = self.delimiter.join([
+            header,
+            '[{0' + space_fmt + '}/{1}]',
+            'eta: {eta}',
+            '{meters}',
+            'time: {time}',
+            'data: {data}',
+            'max mem: {memory:.0f}'
+        ])
+        MB = 1024.0 * 1024.0
+        for obj in iterable:
+            data_time.update(time.time() - end)
+            yield obj
+            iter_time.update(time.time() - end)
+            if i % print_freq == 0:
+                eta_seconds = iter_time.global_avg * (len(iterable) - i)
+                eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+                print(log_msg.format(
+                    i, len(iterable), eta=eta_string,
+                    meters=str(self),
+                    time=str(iter_time), data=str(data_time),
+                    memory=torch.cuda.max_memory_allocated() / MB))
+                sys.stdout.flush()
+
+            i += 1
+            end = time.time()
+        total_time = time.time() - start_time
+        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+        print('{} Total time: {}'.format(header, total_time_str))
+
+
+def mkdir(path):
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
+def setup_for_distributed(is_master):
+    """
+    单卡模式下不需要禁用打印功能
+    """
+    pass
+
+
+def is_dist_avail_and_initialized():
+    """
+    单卡模式下始终返回False
+    """
+    return False
+
+
+def get_world_size():
+    """
+    单卡模式下始终返回1
+    """
+    return 1
+
+
+def get_rank():
+    """
+    单卡模式下始终返回0
+    """
+    return 0
+
+
+def is_main_process():
+    """
+    单卡模式下始终返回True
+    """
+    return True
+
+
+def save_on_master(*args, **kwargs):
+    """
+    单卡模式下直接保存
+    """
+    torch.save(*args, **kwargs)
+
+
+def init_distributed_mode(args):
+    """
+    简化版：设置单卡训练的基本参数
+    """
+    args.distributed = False
+    args.rank = 0
+    args.world_size = 1
+    args.gpu = 0
+
+    # 设置GPU设备
+    torch.cuda.set_device(args.local_rank)
+
+    # 创建输出目录
+    if args.output_dir:
+        mkdir(args.output_dir)
+    if args.model_id:
+        mkdir(os.path.join('./models/', args.model_id))
